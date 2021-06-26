5b9219bf381c32cf40a43c4e928e2f94299515d6
AkhilVinayakp <nirvanaosborn@gmail.com>
2021-06-26 20:20:36 +0530

    max_profit_ single loop update

--- prb_slv/arrays/max_profit.py
+++ prb_slv/arrays/max_profit.py
@@ -3,15 +3,38 @@
 # buying on one day and selling on another.
 
 from typing import List
+import logging 
+# logging.basicConfig(level=logging.DEBUG, forma
 
 st_price = [310, 315, 275, 295, 260, 270,290,230,255,250]
-profit:List = []
 
-for i in range(len(st_price)-1):
-	for j in range(i+1, len(st_price)):
-		if st_price[j] > st_price[i]:
-			profit.append(st_price[j] - st_price[i])
-		else:
-			continue
 
-print(profit, max(profit))
+class MaxProfit:
+	def __init__(self, price_list: List) -> None:
+		self.stock = price_list
+	@property
+	def length(self)->int:
+		return len(self.stock)
+
+	# method 1 bruteforce method
+	def brute_force(self):
+		profit:List = []
+		for i in range(self.length-1):
+			for j in range(i+1, self.length):
+				if self.stock[j] > self.stock[i]:
+					profit.append(self.stock[j]-self.stock[i])
+		return (profit)
+
+	def min_keep(self):
+		profit_list = []
+		min_ = self.stock[0]
+		for i in range(1,self.length):
+			profit_list.append(self.stock[i]-min_ if self.stock[i]> min_ else 0)
+			if self.stock[i] < min_:
+				min_ = self.stock[i]
+		return profit_list
+
+
+t = MaxProfit(st_price)
+print(t.brute_force())
+print(t.min_keep())
