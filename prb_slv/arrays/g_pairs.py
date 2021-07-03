35fccef5394ce79df3281f61889a987065beab1b
AkhilVinayakp <nirvanaosborn@gmail.com>
2021-07-03 23:18:41 +0530

    g_pair test ok

--- prb_slv/arrays/g_pairs.py
+++ prb_slv/arrays/g_pairs.py
@@ -7,17 +7,33 @@ Return the number of good pairs.
 
 '''
 import logging
-from os import name
+# from os import name
 from typing import List
 from collections import Counter
+from itertools import combinations
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s : %(message)s')
 
 class Solution:
+    # @staticmethod
+    # def fact(n:int)->int:
+    #     if n <= 1:
+    #         return 1
+    #     else:
+    #         return n * fact(n-1)
+    @staticmethod
+    def nc2(n:int)->int:
+        return (n * (n -1 ))//(1*2)
     def g_pair(self, nums:List)->int:
-        temp = Counter(nums)
-        print(temp)
-        for i in temp.values():
-            logging.debug(i)
+        counts =  [ i for i in Counter(nums).values()]
+        logging.debug(counts)
+        s = 0
+        for i in counts:
+            if i > 1:
+                s += Solution.nc2(i)
+        print(s)
+        return 0
+
+
 
 if __name__ == "__main__":
     s = Solution()
