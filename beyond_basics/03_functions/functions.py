b2395253b607ef4e9b70cd2d5e29c1332ff29452
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-09-29 21:10:00 +0530

    functional programming

---
+++ beyond_basics/03_functions/functions.py
@@ -0,0 +1,12 @@
+# contains different important  functions available in python
+# %%
+# callable 
+class Test:
+    pass
+print(callable(Test))
+# True since calling the class will invoke the constructor
+fun_a = lambda x: print(x)
+print(callable(fun_a))
+
+
+# %%
