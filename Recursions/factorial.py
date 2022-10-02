8e0bef8b27f67e86dc0fb28400c332f174b8deb1
Akhil Vinayak P <nirvanaosborn@gmail.com>
2022-10-02 23:00:29 +0530

    recursion update, template change

--- Recursions/factorial.py
+++ Recursions/factorial.py
@@ -9,9 +9,13 @@ logger.setLevel(logging.INFO)
 # execution time wrapper
 def track(func):
     @wraps(func)
-    def wrapper(*args):
+    def wrapper(number):
+        if number < 0:
+            '''not defined for negative values'''
+            raise ValueError("not defined for negative values")
+            return None
         st_time = time.time()
-        ret_value = func(*args)
+        ret_value = func(number)
         end_time = time.time()
         logger.info(f"Execution time: {end_time-st_time}")
         num_length = len(str(ret_value))
@@ -20,37 +24,31 @@ def track(func):
         return ret_value
     return wrapper
 
-class Factorial:
-    def __init__(self, num:int) -> None:
-        '''
-        Args: 
-            num(int) : number whoes factorial need to be calculated.
-        Raises:
-            (ValueError) : -ve numbers case.
-        '''
-        if num < 0:
-            raise ValueError("can not instantiate object with a negative number.")
 
-        self.number = num
 
-    @track
-    def fact_iteretive(self):
-        '''
-        Note:
-            factorial using the iterative approach.
+@track
+def fact_iteretive(number):
+    '''
+    Note:
+        factorial using the iterative approach.
 
-        '''
-        if self.number <= 1:
-            return 1
-        else:
-             fact = 1
-             for i in range(2, self.number + 1):
-                fact *= i
-             return fact
+    '''
+    fact = 1
+    for i in range(2, number + 1):
+        fact *= i
+    return fact
 
+@track
+def recursive(number):
+    '''Recursive approch 
+    
+    '''
+    if number <= 1:
+        return 1
+    return recursive(number-1) * number
 
 
 # code execution
 if __name__ == "__main__":
-    f = Factorial(10)
-    print('factorial by iterative approch:', f.fact_iteretive())
+    print('factorial by iterative approach:', fact_iteretive(12))
+    print('factorial by recursive approach:', recursive(12))
