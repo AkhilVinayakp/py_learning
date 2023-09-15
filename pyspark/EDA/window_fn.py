df523a1c69c3313f229c4c1b2cdbf6fe45d950b5
Akhil Vinayak P <nirvanaosborn@gmail.com>
2023-09-15 16:54:03 +0530

    windowing with ranking functions

--- pyspark/EDA/window_fn.py
+++ pyspark/EDA/window_fn.py
@@ -1,5 +1,6 @@
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('Sparkudf').getOrCreate()
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import rank, row_number, dense_rank, ntile, percent_rank
+spark = SparkSession.builder.appName('Spark_windowing').getOrCreate()
 
 
 simpleData = (("James", "Sales", 3000), ("Michael", "Sales", 4600), ("Robert", "Sales", 4100),
@@ -10,4 +11,14 @@ simpleData = (("James", "Sales", 3000), ("Michael", "Sales", 4600), ("Robert", "
 columns = ["employee_name", "department", "salary"]
 df = spark.createDataFrame(data=simpleData, schema=columns)
 df.printSchema()
+df.show(truncate=False)
+
+windowed_data = Window.partitionBy("department").orderBy("Salary")
+
+# applying function over window
+df = df.withColumn("Row number", row_number().over(windowed_data))
+df = df.withColumn("Shallow Rank", rank().over(windowed_data))
+df = df.withColumn("Dense Rank", dense_rank().over(windowed_data))
+df = df.withColumn("Percentage", percent_rank().over(windowed_data))
+df = df.withColumn("nth tile", ntile(n=4).over(windowed_data))
 df.show(truncate=False)
